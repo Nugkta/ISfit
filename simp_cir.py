b9fb21fa427efa292418eced095fff17b12cbc96
Stan <stan.xiao20@imperial.ac.uk>
2022-07-05 21:36:24 +0100

    simplest circuit impedance spectrum

--- simp_cir.py
+++ simp_cir.py
@@ -1,4 +1,46 @@
-import matplotlib as pyplot
+from sre_constants import BRANCH
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def find_imp (w, R1, R2, C):
+    z_r = R1 + R2/(1+(w*R2*C)**2)
+    z_i = -w * R2**2 * C/(1 + (w*R2*C)**2)
+    return z_r, z_i
+
+def plot_spec(w_h, w_l, R1, R2, C):
+    wlist = np.linspace(w_h, w_l, 50000)
+    zrlist = []
+    zilist = []
+    for w in wlist:
+        zr, zi = find_imp(w, R1, R2, C)
+        zrlist.append(zr)
+        zilist.append(-zi)
+    plt.plot(zrlist, zilist,'.')
+    plt.show()
+    return zrlist, zilist
+
+    
+a ,b = plot_spec(0,100,3,4,4)
+a ,b = plot_spec(0,100,0,4,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
