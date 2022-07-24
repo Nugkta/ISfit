dba21a6af3f476c20ad1d0f018eb157331873ed2
Stan <stan.xiao20@imperial.ac.uk>
2022-07-25 00:08:28 +0100

    put in realistic parameters

--- pero2.py
+++ pero2.py
@@ -86,11 +86,11 @@ def find_implist(w_h, w_l,  C_a, C_b, R_i, C_g,  J_s, n, V ,Vb):
 #%%
 #the parameters giving negative impedance
 #a, b, c, d= find_implist(-3, 3, 10,3,10, 1,1,0,2,0)
-a, b, c, d= find_implist(-3, 3, 10,3,10, 1,1,0,2,10)
-
+#a, b, c, d= find_implist(-3, 3, 10,3,10, 1,1,0,2,10)
+#a, b, c, d= find_implist(-3, 3, 10,3,10, 1,1,1.93,2,10)
 # realistic parameters
-
-
+a, b, c, d= find_implist(-3,    3,  2.6e-7,2.6e-7,  3.8e5, 2.8e-8,  7.1e-11,  1.93, 2e-2, 1)
+                        #(w_h, w_l,  C_a, C_b,      R_i, C_g,       J_s,      n, V ,Vb)
 
 
 #a, b, c, d= find_implist(1e-4, 10., 1., 1., 1., 1., 1., 1., 1., 1., 0)  
