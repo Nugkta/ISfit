f80325d79c13a0575b59c4da9c5a2efaf94aa712
Stan <stan.xiao20@imperial.ac.uk>
2022-08-30 23:27:54 +0100

    added sliders for R_ion and other parameters

--- init_guess3.py
+++ init_guess3.py
@@ -39,6 +39,48 @@ from scipy.optimize import fsolve
 #     print( zrlist[-1]  )
 #     return C_a 
 
+VT = 0.026 # = kT/q
+C_a = 2e-7
+C_b = 2e-7
+R_i = 5e8
+Vb = .2
+C_g = 2.8e-8  
+J_s = 7.1e-11
+nA = 1.93
+q = 1.6e-19
+T = 300
+kb = 1.38e-23
+
+class init_guess_class:
+
+    def __init__(self):
+        self.C_A = None
+        self.C_B = None
+        self.R_ion = None
+        self.C_g = None
+        self.J_s = None
+        self.nA = None
+        
+    def update_all(self, init):
+        self.C_A = init[0]
+        self.C_B = init[1]
+        self.R_ion = init[2]
+        self.C_g = init[3]
+        self.J_s = init[4]
+        self.nA = init[5]
+    def update_R_ion(self, R_ion):
+        self.R_ion = R_ion
+    def values(self):
+        return [self.C_A, 
+                self.C_B ,
+                self.R_ion ,
+                self.C_g ,
+                self.J_s ,
+                self.nA ]
+
+
+
+
 def closest_node(node, nodes): #the function for finding the closest point on the line to the user selected point
     #nodes = np.asarray(nodes)
     deltas = nodes - node
@@ -89,23 +131,43 @@ def find_point(df):
 
 
 
-def init_guess(df):
-    R_n8 , R_n0 , w_n , w_t , C_G ,w_r = find_point(df)
+def init_guess(df ,crit_points):
+    R_n8 , R_n0 , w_n , w_t , C_G ,w_r = crit_points
     R_ion = float(input('please input your guess of R_ion: '))
-    J_n = np.real(df['recomb current'][0])
-    print('J_n is ', J_n)
-    J_n_t = R_n8 * q / (1.93 *kb *T)
-    print('theoretical J_n is:',1/J_n_t)
+    J_n = np.real(df['recomb current'][0]) 
+    # print('J_n is ', J_n)
+    # J_n_t = R_n8 * q / (1.93 *kb *T)
+    # print('theoretical J_n is:',1/J_n_t)
     k = R_n0 / R_n8
     n = J_n *R_n8 *q / ( kb *T)
-    C_ion = 1 / (w_n * kb * R_ion)
+    # print(n,J_n ,R_n8 ,q , kb ,T)
+    C_ion = 1 / (w_n * k * R_ion)
     C_g = C_G
     C_A = C_ion / (1 - 1/k)
     C_B = 1 / (1/C_ion - 1/C_A)
-    return C_A,C_B, C_g, R_ion, n
-    
+    V = np.real(df['bias voltage'][0])
+    # print(J_n , V, C_ion,C_A,q,n ,kb ,T)
+    J_s = J_n / np.e**((V*(1 - C_ion/C_A)*q) / (n * kb * T)) #!!!!!!!!!!!!!!!!!!!!! HERE THE VALULE OF N IS OFF
+    return C_A,C_B, R_ion, C_g,  J_s,n
 
 
+def init_guess_slider(df, points, R_ion):       #the function for the slider of R_ion (known points and R_ion)
+    R_n8 , R_n0 , w_n , w_t , C_G ,w_r = points
+    J_n = np.real(df['recomb current'][0]) 
+    # print('J_n is ', J_n)
+    # J_n_t = R_n8 * q / (1.93 *kb *T)
+    # print('theoretical J_n is:',1/J_n_t)
+    k = R_n0 / R_n8
+    n = J_n *R_n8 *q / ( kb *T)
+    # print(n,J_n ,R_n8 ,q , kb ,T)
+    C_ion = 1 / (w_n * k * R_ion)
+    C_g = C_G
+    C_A = C_ion / (1 - 1/k)
+    C_B = 1 / (1/C_ion - 1/C_A)
+    V = np.real(df['bias voltage'][0])
+    # print(J_n , V, C_ion,C_A,q,n ,kb ,T)
+    J_s = J_n / np.e**((V*(1 - C_ion/C_A)*q) / (n * kb * T)) #!!!!!!!!!!!!!!!!!!!!! HERE THE VALULE OF N IS OFF
+    return C_A,C_B, R_ion, C_g,  J_s,n
 
 
 
@@ -144,15 +206,17 @@ df = pd.DataFrame(wz_v_jlist , columns=['frequency' , 'impedance' , 'bias voltag
 
 
 #%% TESTING THE FIND POINT FUNCTION
-R_n8 , R_n0 , w_n , w_t , C_G,w_r = find_point(df)
+# R_n8 , R_n0 , w_n , w_t , C_G,w_r = find_point(df)
 
 
 #%% TESTING THE INIT GUESS FUNCTION
-C_A,C_B, C_g, R_ion,n = init_guess(df)
+# C_A,C_B, C_g, R_ion,n,J_s = init_guess(df)
+# print(C_A,C_B, C_g, R_ion,n,J_s)
 
 
 
-#%%
+
+
 
 """
 the parts below this was to investigate the without-R_ion-guess method of initial guess algorithm.
@@ -168,127 +232,124 @@ outdated for now.
 
 
 
-#%%my own formula for w_t
-C_ion = 1 / (1/C_a + 1/C_b)
-c_ion = C_ion
-R_ion = R_i
-c_g = C_g
-w_t_t = np.sqrt(C_g*(C_g + C_ion))/(R_i * C_g**2 + R_i*C_ion*C_g)
-print(w_t_t)
-w = np.logspace(-8,-2,10000)
+# #%%my own formula for w_t
+# C_ion = 1 / (1/C_a + 1/C_b)
+# c_ion = C_ion
+# R_ion = R_i
+# c_g = C_g
+# w_t_t = np.sqrt(C_g*(C_g + C_ion))/(R_i * C_g**2 + R_i*C_ion*C_g)
+# print(w_t_t)
+# w = np.logspace(-8,-2,10000)
 
 
-def funcb(w):  # the function of theta vs. freqeuncy derived in matlab
-    eq = -(1/(c_ion*w*(R_ion**2*c_g**2*w**2 + 1)) + (R_ion*c_g*(R_ion*c_g*w + R_ion*c_ion*w))/(c_ion*(R_ion**2*c_g**2*w**2 + 1)))/((R_ion*c_g)/(c_ion*(R_ion**2*c_g**2*w**2 + 1)) - (R_ion*c_g*w + R_ion*c_ion*w)/(c_ion*w*(R_ion**2*c_g**2*w**2 + 1)))
-    return eq
+# def funcb(w):  # the function of theta vs. freqeuncy derived in matlab
+#     eq = -(1/(c_ion*w*(R_ion**2*c_g**2*w**2 + 1)) + (R_ion*c_g*(R_ion*c_g*w + R_ion*c_ion*w))/(c_ion*(R_ion**2*c_g**2*w**2 + 1)))/((R_ion*c_g)/(c_ion*(R_ion**2*c_g**2*w**2 + 1)) - (R_ion*c_g*w + R_ion*c_ion*w)/(c_ion*w*(R_ion**2*c_g**2*w**2 + 1)))
+#     return eq
 
 
-plt.plot(w,funcb(w))
-plt.xscale('log')
-plt.yscale('log')
-
-#%%
-
-# plt.plot(np.real(df['frequency'].values), np.angle(np.conjugate(df['impedance']),deg = False),'.')
-# plt.plot(np.real(df['frequency'].values), (np.arctan(-np.imag(df['impedance'])/ np.real(df['impedance']))),)
+# plt.plot(w,funcb(w))
 # plt.xscale('log')
 # plt.yscale('log')
 
-plt.plot(np.real(df['frequency'].values), np.angle(-(df['impedance']),deg = False),'.')
-# plt.plot(np.real(df['frequency'].values), np.angle(np.conjugate(df['impedance']),deg = False),'.')
-#plt.plot(np.real(df['frequency'].values), -(np.arctan(np.imag(df['impedance'])/ np.real(df['impedance']))),)
-plt.xscale('log')
-plt.yscale('log')
-#%%
-c = 1 + 2j
-a5 = np.arctan(np.imag(c)/ np.real(c))
-a6 =  np.angle(c,deg = True)
-print(a5, a6)
-#%%
-C_ion_t = 1/(1/C_a +1/C_b)
-w_t_t = 1/(R_i *np.sqrt(C_g*(C_g +C_ion_t)))
-#%%#%% writing initial guess function, use w_n find c_g
-n_J_n = R_n8 *q /(kb*T)
-k = R_n0 / R_n8
-def func(C_g):
-#def func(C_ion):
-    # R_ion = 1 / (w_n * k * C_ion)
-    # C_g = 1/ (1 / C_G - 1/C_ion)
-    # eq = (R_ion * np.sqrt(C_g * (C_g + C_ion))) - 1 / w_t
-    C_ion = (1/(R_n8*w_r - 1/C_g))
-    #eq=(1/k/(w_n/(R_n8*w_r - 1/C_g))*np.sqrt(np.abs(C_g*(C_g + 1/(R_n8*w_r - 1/C_g)) ))- 1/w_t)
-    eq=(1/k/(w_n*C_ion))*np.sqrt((C_g*(C_g + C_ion) ))- 1/w_t 
-    #print(np.sqrt(C_g * (C_g + C_ion)))
-    # C_ion = (1/(1/C_G -1/C_g))
-    # eq = 1/(k*w_n * C_ion) * ((C_g * (C_g + C_ion)))**0.5 - 1 / w_t
-    #eq = k/(w_n/(R_n8*w_r - 1/C_g))*np.sqrt((C_g*(C_g + 1/(R_n8*w_r - 1/C_g)))) - 1/w_t
-    #eq = np.abs(k/(w_n/(R_n8*w_r - 1/C_g))*np.sqrt(C_g*(C_g + 1/(R_n8*w_r - 1/C_g)) - 1/w_t))
-    return eq
-    # return R_ion * np.sqrt(C_g * (C_g + C_ion)) 
-    
-#%% writing initial guess function, use C_g find c_g
-def func(C_g):
-    C_ion = 1 / (1/C_G - 1/C_g)
-    eq=(1/k/(w_n*C_ion))*np.sqrt((C_g*(C_g + C_ion) ))- 1/w_t 
-    return eq
-    
-#%%
+# #%%
+
+# # plt.plot(np.real(df['frequency'].values), np.angle(np.conjugate(df['impedance']),deg = False),'.')
+# # plt.plot(np.real(df['frequency'].values), (np.arctan(-np.imag(df['impedance'])/ np.real(df['impedance']))),)
+# # plt.xscale('log')
+# # plt.yscale('log')
+
+# plt.plot(np.real(df['frequency'].values), np.angle(-(df['impedance']),deg = False),'.')
+# # plt.plot(np.real(df['frequency'].values), np.angle(np.conjugate(df['impedance']),deg = False),'.')
+# #plt.plot(np.real(df['frequency'].values), -(np.arctan(np.imag(df['impedance'])/ np.real(df['impedance']))),)
+# plt.xscale('log')
+# plt.yscale('log')
+# #%%
+# c = 1 + 2j
+# a5 = np.arctan(np.imag(c)/ np.real(c))
+# a6 =  np.angle(c,deg = True)
+# print(a5, a6)
+# #%%
+# C_ion_t = 1/(1/C_a +1/C_b)
+# w_t_t = 1/(R_i *np.sqrt(C_g*(C_g +C_ion_t)))
+# #%%#%% writing initial guess function, use w_n find c_g
+# n_J_n = R_n8 *q /(kb*T)
+# k = R_n0 / R_n8
 # def func(C_g):
-#     C_ion = 1/(R_n8*w_r - 1/C_g)
-#     eq=(C_g*(C_g + C_ion))
-#     return C_ion
+# #def func(C_ion):
+#     # R_ion = 1 / (w_n * k * C_ion)
+#     # C_g = 1/ (1 / C_G - 1/C_ion)
+#     # eq = (R_ion * np.sqrt(C_g * (C_g + C_ion))) - 1 / w_t
+#     C_ion = (1/(R_n8*w_r - 1/C_g))
+#     #eq=(1/k/(w_n/(R_n8*w_r - 1/C_g))*np.sqrt(np.abs(C_g*(C_g + 1/(R_n8*w_r - 1/C_g)) ))- 1/w_t)
+#     eq=(1/k/(w_n*C_ion))*np.sqrt((C_g*(C_g + C_ion) ))- 1/w_t 
+#     #print(np.sqrt(C_g * (C_g + C_ion)))
+#     # C_ion = (1/(1/C_G -1/C_g))
+#     # eq = 1/(k*w_n * C_ion) * ((C_g * (C_g + C_ion)))**0.5 - 1 / w_t
+#     #eq = k/(w_n/(R_n8*w_r - 1/C_g))*np.sqrt((C_g*(C_g + 1/(R_n8*w_r - 1/C_g)))) - 1/w_t
+#     #eq = np.abs(k/(w_n/(R_n8*w_r - 1/C_g))*np.sqrt(C_g*(C_g + 1/(R_n8*w_r - 1/C_g)) - 1/w_t))
+#     return eq
+#     # return R_ion * np.sqrt(C_g * (C_g + C_ion)) 
+    
+# #%% writing initial guess function, use C_g find c_g
+# def func(C_g):
+#     C_ion = 1 / (1/C_G - 1/C_g)
+#     eq=(1/k/(w_n*C_ion))*np.sqrt((C_g*(C_g + C_ion) ))- 1/w_t 
+#     return eq
+    
+# #%%
+# # def func(C_g):
+# #     C_ion = 1/(R_n8*w_r - 1/C_g)
+# #     eq=(C_g*(C_g + C_ion))
+# #     return C_ion
 
     
- #%%   writing initial guess function, use C_g find c_ion
-def func(C_ion):
-    R_ion = 1 / (w_n * k * C_ion)
-    C_g = 1/ (1 / C_G - 1/C_ion)
-    #eq = (R_ion * np.sqrt((C_g * (C_g + C_ion)))) - 1 / w_t
-    eq = (R_ion * np.sqrt((C_g * (C_g + C_ion)))) - 1 / w_t
-    return eq     
-    
-#%%writing function testing is C_g goes negaitve
+#  #%%   writing initial guess function, use C_g find c_ion
 # def func(C_ion):
 #     R_ion = 1 / (w_n * k * C_ion)
 #     C_g = 1/ (1 / C_G - 1/C_ion)
+#     #eq = (R_ion * np.sqrt((C_g * (C_g + C_ion)))) - 1 / w_t
 #     eq = (R_ion * np.sqrt((C_g * (C_g + C_ion)))) - 1 / w_t
-#     return C_g    
+#     return eq     
+    
+# #%%writing function testing is C_g goes negaitve
+# # def func(C_ion):
+# #     R_ion = 1 / (w_n * k * C_ion)
+# #     C_g = 1/ (1 / C_G - 1/C_ion)
+# #     eq = (R_ion * np.sqrt((C_g * (C_g + C_ion)))) - 1 / w_t
+# #     return C_g    
 
     
-#%%
-root = fsolve(func, 1e-7)
-print(root)
-#%%
-# C_g = np.logspace(-10,10,10000)
-C_ion = np.logspace(-8,-2,10000)
-list9 = np.ones(10000)*10
-# plt.plot(C_ion , func(C_ion)-0.9
-plt.plot(C_ion , func(C_ion)+10)
-plt.plot(C_ion , list9) 
-plt.xscale('log')
-plt.yscale('log')
+# #%%
+# root = fsolve(func, 1e-7)
+# print(root)
+# #%%
+# # C_g = np.logspace(-10,10,10000)
+# C_ion = np.logspace(-8,-2,10000)
+# list9 = np.ones(10000)*10
+# # plt.plot(C_ion , func(C_ion)-0.9
+# plt.plot(C_ion , func(C_ion)+10)
+# plt.plot(C_ion , list9) 
+# plt.xscale('log')
+# plt.yscale('log')
 
 
-#%% theoretical values
-C_i = 1/(1/C_a +1/C_b)
-C_G_t = 1/(1/C_i +1/C_g)
+# #%% theoretical values
+# C_i = 1/(1/C_a +1/C_b)
+# C_G_t = 1/(1/C_i +1/C_g)
 
 
-plt.plot(df['frequency'], np.real(df['impedance']))
+# plt.plot(df['frequency'], np.real(df['impedance']))
 
-plt.xscale('log')
-plt.yscale('log')
-
-
-
+# plt.xscale('log')
+# plt.yscale('log')
 
 
 
-#%%
-C_im =1/ (R_n8 *w_r - 1/4.7973e-05)
 
 
 
+# #%%
+# C_im =1/ (R_n8 *w_r - 1/4.7973e-05)
 
 
 
@@ -314,147 +375,150 @@ C_im =1/ (R_n8 *w_r - 1/4.7973e-05)
 
 
 
-#%% PROCEDURE of finding the initial guess
-#这里是我在尝试用它
-from scipy.optimize import fsolve
 
 
 
-# def picking():
-#     pick_extremum(zrlist,zilist, 'max')
-#     pick_extremum(zrlist,zilist, 'min')
-
-
-#preparing for finding the critical points on the plot 
-wzlist = df[['frequency','impedance']].to_numpy()
-zrlist = np.real(df['impedance'].values)
-zilist = np.imag(df['impedance'].values)
-zrilist = np.stack((zrlist , -zilist), axis = 1)
-#now letting thr user to determine the peak and local minimum
-plt.plot(zrlist,-zilist,'.')
-plt.title('please select the vertices')
-vertices = plt.ginput(2)
-plt.close()
-
-
-plt.plot(zrlist,-zilist,'.')
-plt.title('please select the minimum')
-mini = plt.ginput(1)
-plt.close()
-# use the approximate position the user selected to find the corresponding index in the dataframe
-def closest_node(node, nodes):
-    #nodes = np.asarray(nodes)
-    deltas = nodes - node
-    dist_2 = np.einsum('ij,ij->i', deltas, deltas)
-    return np.argmin(dist_2)
-
-nhlist = []
-nllist = []
-
-for vert in vertices:
-    num = closest_node(vert , zrilist)
-    nhlist.append(num)
-
-minnum = closest_node(mini , zrilist)
-nllist.append(minnum)
-nllist = np.asarray(nllist)
-nhlist = np.asarray(nhlist)
-
-
-# pick_extremum(zrlist,zilist, 'max')
-# pick_extremum(zrlist,zilist, 'min')
-# picking()
-# def is_continue(cont):
-#     if cont == 'y':
-#         return True 
-#     return False
-# cont = input('are you ready to continue? y/n \n')
-# wait(lambda: is_continue(cont), timeout_seconds=120, waiting_for="something to be ready")
-# nhlist = np.loadtxt('vertices.txt',delimiter = ',')
-# nllist = np.loadtxt('mins.txt',delimiter = ',')
+# #%% PROCEDURE of finding the initial guess
+# #这里是我在尝试用它
+# from scipy.optimize import fsolve
 
 
 
+# # def picking():
+# #     pick_extremum(zrlist,zilist, 'max')
+# #     pick_extremum(zrlist,zilist, 'min')
 
-# nhlist2 = np.array(argrelextrema(zilist, np.less))[0]
-# nllist2 = np.array(argrelextrema(wzlist[:,1].imag, np.greater))[0]
-whlist = np.real(wzlist[nhlist][: , 0])
 
-# First extract all the needed information from the plot
-R_n0 = zrlist[-1]   #the last element of zrlist, corresponds to the end of the curve. Might need to revise for imcomplete circle
-R_n8 = wzlist[nllist[0]][1].real
-wn = min(whlist)
-wr = max(whlist)
-wt = wzlist[[nllist][0]][0][0].real  #w_theta
+# #preparing for finding the critical points on the plot 
+# wzlist = df[['frequency','impedance']].to_numpy()
+# zrlist = np.real(df['impedance'].values)
+# zilist = np.imag(df['impedance'].values)
+# zrilist = np.stack((zrlist , -zilist), axis = 1)
+# #now letting thr user to determine the peak and local minimum
+# plt.plot(zrlist,-zilist,'.')
+# plt.title('please select the vertices')
+# vertices = plt.ginput(2)
+# plt.close()
 
-#Now start to implement the finding algo
-k = R_n8 / R_n0
-#solving the three variable non linear equation.
-def equations(p, R_n0 , R_n8 , wn , wr , wt , k ):
-    C_ion ,  C_g = p
-    eq1 = 1 / (R_n8 *wr - 1 / C_g) - C_ion
-    eq2 = k / (wn * C_ion) * np.sqrt(C_g * (C_g + C_ion)) - 1/wt
-    return (eq1 , eq2 )
-#%%
-C_ion , C_g = fsolve(lambda p: equations(p, R_n0 , R_n8 , wn , wr , wt , k ), (1 , 1) , maxfev= 1000000 )
+
+# plt.plot(zrlist,-zilist,'.')
+# plt.title('please select the minimum')
+# mini = plt.ginput(1)
+# plt.close()
+# # use the approximate position the user selected to find the corresponding index in the dataframe
+# def closest_node(node, nodes):
+#     #nodes = np.asarray(nodes)
+#     deltas = nodes - node
+#     dist_2 = np.einsum('ij,ij->i', deltas, deltas)
+#     return np.argmin(dist_2)
+
+# nhlist = []
+# nllist = []
+
+# for vert in vertices:
+#     num = closest_node(vert , zrilist)
+#     nhlist.append(num)
+
+# minnum = closest_node(mini , zrilist)
+# nllist.append(minnum)
+# nllist = np.asarray(nllist)
+# nhlist = np.asarray(nhlist)
+
+
+# # pick_extremum(zrlist,zilist, 'max')
+# # pick_extremum(zrlist,zilist, 'min')
+# # picking()
+# # def is_continue(cont):
+# #     if cont == 'y':
+# #         return True 
+# #     return False
+# # cont = input('are you ready to continue? y/n \n')
+# # wait(lambda: is_continue(cont), timeout_seconds=120, waiting_for="something to be ready")
+# # nhlist = np.loadtxt('vertices.txt',delimiter = ',')
+# # nllist = np.loadtxt('mins.txt',delimiter = ',')
+
+
+
+
+# # nhlist2 = np.array(argrelextrema(zilist, np.less))[0]
+# # nllist2 = np.array(argrelextrema(wzlist[:,1].imag, np.greater))[0]
+# whlist = np.real(wzlist[nhlist][: , 0])
+
+# # First extract all the needed information from the plot
+# R_n0 = zrlist[-1]   #the last element of zrlist, corresponds to the end of the curve. Might need to revise for imcomplete circle
+# R_n8 = wzlist[nllist[0]][1].real
+# wn = min(whlist)
+# wr = max(whlist)
+# wt = wzlist[[nllist][0]][0][0].real  #w_theta
+
+# #Now start to implement the finding algo
+# k = R_n8 / R_n0
+# #solving the three variable non linear equation.
+# def equations(p, R_n0 , R_n8 , wn , wr , wt , k ):
+#     C_ion ,  C_g = p
+#     eq1 = 1 / (R_n8 *wr - 1 / C_g) - C_ion
+#     eq2 = k / (wn * C_ion) * np.sqrt(C_g * (C_g + C_ion)) - 1/wt
+#     return (eq1 , eq2 )
+# #%%
+# C_ion , C_g = fsolve(lambda p: equations(p, R_n0 , R_n8 , wn , wr , wt , k ), (1 , 1) , maxfev= 1000000 )
     
 
 
 
-print(C_ion , C_g)
+# print(C_ion , C_g)
 
 
-#%% then trying out the functions
-C_g = 2.8e-8  
-C_ion = 1/ (1/C_a +1/C_b)
+# #%% then trying out the functions
+# C_g = 2.8e-8  
+# C_ion = 1/ (1/C_a +1/C_b)
 
-print(1 / (R_n8 *wr - 1 / C_g) - C_ion)
+# print(1 / (R_n8 *wr - 1 / C_g) - C_ion)
 
-#%%trying the interactive point picking function
+# #%%trying the interactive point picking function
 
-def pick_vertex(zrlist,zilist):
-    myfile = open('vertices.txt' , 'w')
-    myfile.close()
-    # simple picking, lines, rectangles and text
-    fig, ax1 = plt.subplots(1, 1)
-    ax1.set_title('Please click on the vertices of the Nyquist plot', picker=True)
-    ax1.set_ylabel('ylabel', picker=True, bbox=dict(facecolor='red'))
-    line, = ax1.plot(zrlist,-zilist, 'o', picker=True, pickradius=5)
+# def pick_vertex(zrlist,zilist):
+#     myfile = open('vertices.txt' , 'w')
+#     myfile.close()
+#     # simple picking, lines, rectangles and text
+#     fig, ax1 = plt.subplots(1, 1)
+#     ax1.set_title('Please click on the vertices of the Nyquist plot', picker=True)
+#     ax1.set_ylabel('ylabel', picker=True, bbox=dict(facecolor='red'))
+#     line, = ax1.plot(zrlist,-zilist, 'o', picker=True, pickradius=5)
 
-    # pick the rectangle
-    # ax2.bar(range(10), rand(10), picker=True)
-    # for label in ax2.get_xticklabels():  # make the xtick labels pickable
-    #     label.set_picker(True)
-    def onpick1(event):
-        if isinstance(event.artist, Line2D):
-            thisline = event.artist
-            xdata = thisline.get_xdata()
-            ydata = thisline.get_ydata()
-            ind = event.ind
-            top = (np.column_stack([xdata[ind], -ydata[ind]])[0])
-            print('onpick1 line:', top)
-            myfile = open('vertices.txt' , 'a')
-            myfile.write(str(top)+'\n')
-            myfile.close()
-            #return np.column_stack([xdata[ind], ydata[ind]])
-        # elif isinstance(event.artist, Rectangle):
-        #     patch = event.artist
-        #     print('onpick1 patch:', patch.get_path())
-        # elif isinstance(event.artist, Text):
-        #     text = event.artist
-        #     print('onpick1 text:', text.get_text())
+#     # pick the rectangle
+#     # ax2.bar(range(10), rand(10), picker=True)
+#     # for label in ax2.get_xticklabels():  # make the xtick labels pickable
+#     #     label.set_picker(True)
+#     def onpick1(event):
+#         if isinstance(event.artist, Line2D):
+#             thisline = event.artist
+#             xdata = thisline.get_xdata()
+#             ydata = thisline.get_ydata()
+#             ind = event.ind
+#             top = (np.column_stack([xdata[ind], -ydata[ind]])[0])
+#             print('onpick1 line:', top)
+#             myfile = open('vertices.txt' , 'a')
+#             myfile.write(str(top)+'\n')
+#             myfile.close()
+#             #return np.column_stack([xdata[ind], ydata[ind]])
+#         # elif isinstance(event.artist, Rectangle):
+#         #     patch = event.artist
+#         #     print('onpick1 patch:', patch.get_path())
+#         # elif isinstance(event.artist, Text):
+#         #     text = event.artist
+#         #     print('onpick1 text:', text.get_text())
 
-    fig.canvas.mpl_connect('pick_event', onpick1)
+#     fig.canvas.mpl_connect('pick_event', onpick1)
 
  
-#pick_simple(zrlist,zilist)
+# #pick_simple(zrlist,zilist)
 
 
 
-#%% reading the string in file to be np array
-#y = np.loadtxt('vertices.txt',delimiter = ',')
+# #%% reading the string in file to be np array
+# #y = np.loadtxt('vertices.txt',delimiter = ',')
  
-#nhlist = get_points('vertices.txt')
+# #nhlist = get_points('vertices.txt')
 
 
 
--- read_fit_with0V_2.py
+++ read_fit_with0V_2.py
@@ -57,50 +57,6 @@ dfs
 
 
 
-
-
-
-
-
-#%%
-init_guess = pif.get_init_guess(dfs)
-# init_guess = ig3.init_guess(df)
-
-
-
-
-
-
-
-#%%
-#init_guess = [5.355788154685985e-05, 2.877587419452046e-05, 117807.98943844388, 3.221211845752507e-07, 6.343209119597507e-23, 1.5]
-#init_guess = [5.355788154685985e-05,
- # 2.877587419452046e-05,
- # 117807.98943844388,
- # 3.221211845752507e-07,
- # 1.6424828411455001e-22,
- # 1.5]
-
-#%% testing global fit
-
-popt, pcov = pif.global_fit(dfs , init_guess)
-
-
-v = [0,.795,.864,.894]
-wlist = np.logspace(-6,6,1000)
-
-for i in v:
-    z , j = pif.pero_model(wlist,*popt,i)
-    plt.plot(np.real(z),-np.imag(z),'--')
-
-for i in range(0,4):
-    df = dfs[i]
-    plt.plot(np.real(df['impedance'].values), -np.imag(df['impedance']),'.')
-
-plt.xlim([-300,3000])
-plt.ylim([-200,1000])
-
-
 #%% testing individual fit
 #AND adding the slider function for changing the initial guess
 
@@ -111,12 +67,201 @@ plt.ylim([-200,1000])
 from matplotlib.widgets import Slider, Button
 wlist = np.logspace(-6,6,1000)
 
-a = 3 #change this to change the set of data to fit
-v = [0,.795,.864,.894]
+a = 2 #change this to change the set of data to fit
+v = [0,.795,.846,.894]
+
+crit_points = ig3.find_point(dfs[a])
+
+iglist = ig3.init_guess(dfs[a],crit_points) #getting the initial guess by ig3 functions
+init_guess = ig3.init_guess_class()
+init_guess.update_all(iglist)
+
+print(init_guess.values())
 
 
-# init_guess = ig3.init_guess(df)
 
+
+#%% PLOTTING OUT THE INITIAL GUESS AS MODEL INPUT TO SEE DEVIATION POSSIBLY ADD SLIDER LATER
+df = dfs[a]
+
+plt.plot(np.real(df['impedance']) , -np.imag(df['impedance']) , 'g.')
+# obtaining the Nyquist plot with initial guess as input to see the goodness of fit initially
+simu_Z, simu_J1 = pif.pero_model(wlist,*init_guess.values(),v[a])
+plt.plot(simu_Z.real , -simu_Z.imag )
+plt.title('R_ion = 1e6')
+
+#%% try to add the slider for R_ion
+fig , ax = plt.subplots(figsize = (8,5), ncols = 1 , nrows = 1) #opening the canvas for the plot of Nyquist plot
+line1 = plt.plot(np.real(df['impedance'].values), -np.imag(df['impedance']),'x', ms=4)
+line2, = plt.plot(np.real(simu_Z),-np.imag(simu_Z),'r--')
+ax.set_xlabel('Z\'')
+ax.set_ylabel('Z\'\'')
+plt.subplots_adjust(left=0.15, bottom=.2)          #adjusting the position of the main plot to leave room for then
+
+ax_r = plt.axes([0.25, 0.07, 0.55, 0.03])
+
+R_slider = Slider(
+    ax = ax_r, 
+    label = 'R_ion',
+    valmin = np.log(0.05 * init_guess.values()[2]),
+    valmax = np.log(20 * init_guess.values()[2]),
+    valinit = np.log(init_guess.values()[2]),
+    )
+
+R_slider.valtext.set_text('%.2e'%init_guess.values()[2])
+
+
+def update_R_ion(val,points):
+    R_ion = np.exp(R_slider.val)
+    iglist = ig3.init_guess_slider(dfs[a],points,R_ion)
+    init_guess.update_all(iglist)
+    simu_Z, simu_J1 = pif.pero_model(wlist,*init_guess.values(),v[a])
+    line2.set_ydata(-np.imag(simu_Z))
+    line2.set_xdata(np.real(simu_Z))
+    amp = np.exp(R_slider.val)
+    R_slider.valtext.set_text('%.2e'%amp)
+    fig.canvas.draw_idle()
+
+
+# R_ion = Updated()
+# def on_change(v):
+#     val = np.exp(R_slider.val)
+#     R_ion.update(val)
+
+
+R_slider.on_changed(lambda val: update_R_ion(val , crit_points))
+# R_slider.on_changed(on_change)
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button_R = Button(resetax, 'Reset', hovercolor='0.975')
+def reset_R_ion(event):
+    R_slider.reset()
+button_R.on_clicked(reset_R_ion)
+
+
+
+
+# NOW TRY TO ADD SLIDERS FOR ALL PARAMETERS IN THE INITIAL GUESSS
+
+#notet that the R_ion here causes change differenetly from the previous slider because the previous R will cause change on Jn and nA simultaneously but the R_ion is not connected to the other parameters.
+
+
+
+
+#%%
+simu_Z, simu_J1 = pif.pero_model(wlist,*init_guess.values(),v[a])
+fig, ax = plt.subplots(figsize=(5, 5),ncols = 1 , nrows = 1)
+
+line1 = plt.plot(np.real(df['impedance'].values), -np.imag(df['impedance']),'x', ms=4)
+line2, = plt.plot(np.real(simu_Z),-np.imag(simu_Z),'r--')
+ax.set_xlabel('Z\'')
+ax.set_ylabel('Z\'\'')
+plt.subplots_adjust(left=0.25, bottom=.5)
+#change only the C_a in popt now as a test
+# axC_a = plt.axes([0.25, 0.5, 0.65, 0.03])
+ax_list = {} 
+sliders = {}
+param_name = ['C_a', 'C_b', 'R_i', 'C_g', 'J_s', 'nA' ]
+
+
+
+
+for i in range(0,6):
+    ax_list[i] = plt.axes([0.25, 0.05 * (i+2)-0.02, 0.55, 0.03])
+    sliders[i] = Slider(
+        ax = ax_list[i], 
+        label = 'the value of ' + param_name[i],
+        valmin = 1/3 * init_guess.values()[i],
+        valmax = 3 * init_guess.values()[i],
+        valinit = init_guess.values()[i],
+        )
+
+sl_val_list =[]
+for key in sliders:
+    sl_val_list.append(sliders[key])
+
+def update(val,  ):
+    # popt = np.delete(popt , i)
+    # popt = np.insert(popt,i,sliders[i].val)
+    vals = [i.val for i in sl_val_list]
+    init_guess.update_all(vals)
+    z , j = pif.pero_model(wlist,*vals,v1)
+    # print(vals)
+    # plt.figure()
+    # plt.plot(np.real(z), -np.imag(z),'x', ms=4)
+    #z , j = pif.pero_model(wlist,sliders[0].val,sliders[1].val,sliders[2].val,sliders[3].val,sliders[4].val,sliders[5].val,v1)
+    #z , j = pif.pero_model(wlist,*popt,v1)
+    line2.set_ydata(-np.imag(z))
+    line2.set_xdata(np.real(z))
+    fig.canvas.draw_idle()
+    
+
+for key in sliders:
+    #print(type(sliders[key]))
+    sliders[key].on_changed(lambda val: update(val))
+    
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button = Button(resetax, 'Reset', hovercolor='0.975')
+def reset(event):
+    for key in sliders:
+        sliders[key].reset()
+button.on_clicked(reset)
+plt.show()
+
+
+
+
+#%% PUTTING THE FIT AND THE INIT GUESS TOGETHER TO COMPARE
+popt, pcov = pif.global_fit([dfs[a]] , init_guess.values())
+df = dfs[a]
+v1 = v[a]
+z , j = pif.pero_model(wlist,*popt,v1)
+z_ig, j_ig = pif.pero_model(wlist,*init_guess.values(),v1)
+fig, axs = plt.subplots(figsize=(5, 5),ncols = 1 , nrows = 1)
+ax = axs
+line1 = plt.plot(np.real(df['impedance'].values), -np.imag(df['impedance']),'x', ms=4)
+line2, = plt.plot(np.real(z),-np.imag(z),'r--')
+line3 = plt.plot(np.real(z_ig),-np.imag(z_ig),'b--')
+ax.set_xlabel('Z\'')
+ax.set_ylabel('Z\'\'')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+#%%
 
 popt, pcov = pif.global_fit([dfs[a]] , init_guess)
 # popt, pcov = pif.global_fit([dfs[1]] , init_guess)
@@ -168,9 +313,8 @@ def update(val,  ):
     # plt.plot(np.real(z), -np.imag(z),'x', ms=4)
     #z , j = pif.pero_model(wlist,sliders[0].val,sliders[1].val,sliders[2].val,sliders[3].val,sliders[4].val,sliders[5].val,v1)
     #z , j = pif.pero_model(wlist,*popt,v1)
-    line2.set_ydata(-np.imag(z))
-    line2.set_xdata(np.real(z))
-    fig.canvas.draw_idle()
+
+    
 
 for key in sliders:
     #print(type(sliders[key]))
