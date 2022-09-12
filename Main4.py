6e6e886adc1c88c58044c819759d1861c5dfae49
Stan <stan.xiao20@imperial.ac.uk>
2022-09-12 13:58:02 +0100

    tested

--- Main4.py
+++ Main4.py
@@ -43,44 +43,7 @@ v = v[a]
 
 
 
-
-
-# crit_points = igp.find_point(df)
-# ig = igp.init_guess_find(df,crit_points) 
-# init_guess = igp.init_guess_class()
-# init_guess.update_all(ig)
-
 #%%
-# def __main__(dfs,v):
-#     df = dfs[-1]#only uses the last plot to find the initial guess (becasue it has the stable shape)
-#     crit_points = find_point(dfs[-1]) 
-#     ig = init_guess_find(df,crit_points) 
-#     init_guess = init_guess_class()
-#     init_guess.update_all(ig)
-#     R_ion_Slider(init_guess, df, v,crit_points)
-
-df = dfs[-1]
-crit_points = igp.find_point(dfs[-1]) 
-ig = igp.init_guess_find(df,crit_points)
-init_guess = igp.init_guess_class()
-init_guess.update_all(ig)
-#%%
-#igp.fit_plot_comp_plots(dfs,init_guess)
-
-
-#%%
-# zlist_big = np.array([])      #because we are fitting the w,v to z, need to stack up all the w v and z list from different Vb each to be one big list. 
-# wlist_big = np.array([])
-# vlist_big = np.array([])
-# for df in dfs:
-#     zlist_big = np.concatenate((zlist_big , df['impedance'].values))
-#     wlist_big = np.concatenate((wlist_big , df['frequency'].values.real))
-#     vlist_big = np.concatenate((vlist_big , df['bias voltage'].values.real))
-# wvlist_big = np.stack((wlist_big,vlist_big),axis = 1)
-# z = pmf.pero_sep(wvlist_big,*init_guess.values(), 1, 1)
-
-# plt.plot(z[0:144],-z[144:288],'.')
-
 
 igp.__main__(dfs)
 
